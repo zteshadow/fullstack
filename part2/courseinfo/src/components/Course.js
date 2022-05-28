5209fdb147327b5205f2b96f6095893308233cce
zteshadow <samuel.song.bc@gmail.com>
2022-05-28 11:07:56 +0800

    2.1 done

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,8 +1,29 @@
 
 import React from 'react'
 
-const Course = () => {
 
+const Header = ({text}) => <h1>{text}</h1>
+const Part = ({part}) => <p>{part.name} {part.exercises}</p>
+  
+const Content = ({parts}) => {
+    console.log(parts);
+    
+    return (
+        <div>
+            {parts.map(part =>
+                <Part key={part.id} part={part} />    
+            )}
+        </div>
+    )
+}
+  
+const Course = ({course}) => {
+    return (
+        <>
+        <Header text={course.name} />
+        <Content parts={course.parts} />
+        </>
+    )
 }
 
 export default Course

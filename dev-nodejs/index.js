bb7172de0d98e907ce249d431c26e414bfe7f5ad
zteshadow <samuel.song.bc@gmail.com>
2022-06-03 10:24:06 +0800

    refine

--- dev-nodejs/index.js
+++ dev-nodejs/index.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
   {
     id: 1,
@@ -54,8 +56,26 @@ app.delete('/api/notes/:id', (request, response) => {
   }
 })
 
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+  if (!body.content) {
+    return response.status(400).json({error: 'content missing'})
+  }
+
+  const maxId = notes.length > 0 ? Math.max(...notes.map(note => note.id)) : 0
+  const note = {
+    id: maxId + 1,
+    content: body.content,
+    date: Date(),
+    important: body.important || false
+  }
+
+  notes = notes.concat(note)
+  //console.log(note)
+  response.json(note)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-

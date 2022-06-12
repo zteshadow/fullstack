4ad0dc760643b3ebcf8d24ce9503ac2a734dea92
zteshadow <samuel.song.bc@gmail.com>
2022-06-12 09:32:39 +0800

    add filter

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,18 @@
 
 import { useState } from 'react'
-import Person from "./components/Person"
+import Person from './components/Person'
+import Filter from './components/Filter'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '123-456' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState("")
+  const [filter, setFilter] = useState("")
 
   const handleAdd = (evt) => {
     evt.preventDefault()
@@ -46,12 +51,14 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter value={filter} onChange={(evt) => setFilter(evt.target.value)} />
+      <h3>Add a new</h3>
       <form onSubmit={handleAdd}>
         <div>name: <input value={newName} onChange={handleNameInput}/></div>
         <div>number: <input value={newNumber} onChange={handleNumberInput}/></div>
         <div><button type="submit">add</button></div>
       </form>
-      <h2>Numbers</h2>
+      <h3>Numbers</h3>
       <ul>
         {persons.map(person =>
           <Person key={person.name} person={person} />

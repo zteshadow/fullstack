c12dcd1a8356c98fa804b90dcf16ffa098e0fbb7
zteshadow <samuel.song.bc@gmail.com>
2022-06-18 13:21:35 +0800

    country wip

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,8 +1,22 @@
 
 import React from 'react'
 
-const Country = () => {
-
+const Country = ({country}) => {
+    console.log('languages', country.languages, typeof country.languages)
+    
+    return (
+        <>
+        <h2>{country.name.common}</h2>
+        <div>
+            <div>capital {country.capital}</div>
+            <div>area {country.area}</div>
+        </div>
+        <h3>languages:</h3>
+        <ul>
+            {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
+        </ul>
+        </>
+    )
 }
 
 export default Country
--- part2/countries/src/components/CountryList.js
+++ part2/countries/src/components/CountryList.js
@@ -1,20 +1,25 @@
 
 import React from 'react'
+import Country from './Country'
 
 const CountryList = ({countries, filterEnabled}) => {
     console.log('filter: ', filterEnabled)
     
     if (filterEnabled && countries.length > 10) {
         return (<div>Too many matches, spedify another filter</div>)
-    } 
+    }
     
-    return (
-        <ul>
-        {countries.map(country =>
-            <div key={country.name.common}> {country.name.common} </div>
-        )}
-        </ul>
-    )
+    if (countries.length == 1) {
+        return <Country country={countries[0]} />
+    } else {
+        return (
+        <>
+            {countries.map(country =>
+                <div key={country.name.common}> {country.name.common} </div>
+            )}
+        </>
+        )
+    }
 }
 
 export default CountryList

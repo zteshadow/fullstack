61a867a58177f1d291bc7be2c19f87c0df8be6e4
zteshadow <samuel.song.bc@gmail.com>
2022-07-02 16:09:07 +0800

    inline style done

--- test/src/App.js
+++ test/src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import noteService from './services/notes'
 import Note from "./components/Note"
 import Notification from './components/Notification'
+import Footer from './components/Footer'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -82,6 +83,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange}/>
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   )
 }
